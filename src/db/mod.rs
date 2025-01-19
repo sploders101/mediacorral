65a7857b9ca5b69aa1db1fd8d83c65720b11e217
Shaun Keys <mariobuddy@gmail.com>
2025-01-18 21:36:19 -0700

    Add insert functions

---
+++ src/db/mod.rs
@@ -0,0 +1,278 @@
+use std::convert::Infallible;
+
+use schemas::*;
+use sqlx::Executor;
+
+pub mod schemas;
+
+type Db = sqlx::SqlitePool;
+
+pub async fn insert_movie(db: &Db, movie: &MoviesItem) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO movies (
+                id,
+                tmdb_id,
+                poster_blob,
+                title,
+                description
+            ) VALUES (?, ?, ?, ?, ?)
+        ",
+        movie.id,
+        movie.tmdb_id,
+        movie.poster_blob,
+        movie.title,
+        movie.description,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_movies_special_feature(
+    db: &Db,
+    movie_special_feature: &MoviesSpecialFeaturesItem,
+) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO movies_special_features (
+                id,
+                movie_id,
+                thumbnail_blob,
+                title,
+                description
+            ) VALUES (?, ?, ?, ?, ?)
+        ",
+        movie_special_feature.id,
+        movie_special_feature.movie_id,
+        movie_special_feature.thumbnail_blob,
+        movie_special_feature.title,
+        movie_special_feature.description,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_tv_show(db: &Db, tv_show: &TvShowsItem) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO tv_shows (
+                id,
+                tmdb_id,
+                poster_blob,
+                title,
+                description
+            ) VALUES (?, ?, ?, ?, ?)
+        ",
+        tv_show.id,
+        tv_show.tmdb_id,
+        tv_show.poster_blob,
+        tv_show.title,
+        tv_show.description,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_tv_season(db: &Db, tv_season: &TvSeasonsItem) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO tv_seasons (
+                id,
+                tv_show_id,
+                season_number,
+                poster_blob,
+                title,
+                description
+            ) VALUES (?, ?, ?, ?, ?, ?)
+        ",
+        tv_season.id,
+        tv_season.tv_show_id,
+        tv_season.season_number,
+        tv_season.poster_blob,
+        tv_season.title,
+        tv_season.description,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_tv_episode(db: &Db, tv_episode: &TvEpisodesItem) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO tv_episodes (
+                id,
+                tv_show_id,
+                tv_season_id,
+                episode_number,
+                thumbnail_blob,
+                title,
+                description
+            ) VALUES (?, ?, ?, ?, ?, ?, ?)
+        ",
+        tv_episode.id,
+        tv_episode.tv_show_id,
+        tv_episode.tv_season_id,
+        tv_episode.episode_number,
+        tv_episode.thumbnail_blob,
+        tv_episode.title,
+        tv_episode.description,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_movie_file(db: &Db, movie_file: &MovieFilesItem) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO movie_files (
+                blob_id,
+                movie_id
+            ) VALUES (?, ?)
+        ",
+        movie_file.blob_id,
+        movie_file.movie_id,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_movie_special_features_file(
+    db: &Db,
+    movie_special_features_file: &MovieSpecialFeaturesFilesItem,
+) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO movie_special_features_files (
+                blob_id,
+                movie_id
+            ) VALUES (?, ?)
+        ",
+        movie_special_features_file.blob_id,
+        movie_special_features_file.movie_id,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_tv_file(db: &Db, tv_file: &TvFilesItem) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO tv_files (
+                blob_id,
+                tv_show_id,
+                tv_season_id,
+                tv_episode_id
+            ) VALUES (?, ?, ?, ?)
+        ",
+        tv_file.blob_id,
+        tv_file.tv_show_id,
+        tv_file.tv_season_id,
+        tv_file.tv_episode_id,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_untagged_media(
+    db: &Db,
+    untagged_media: &UntaggedMediaItem,
+) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO untagged_media (blob_id, subtitle_id)
+            VALUES (?, ?)
+        ",
+        untagged_media.blob_id,
+        untagged_media.subtitle_id,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_video_meta(
+    db: &Db,
+    video_meta: &VideoMetadataItem,
+) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO video_metadata (
+                blob_id,
+                resolution,
+                resolution_width,
+                resolution_height,
+                video_format,
+                length,
+                audio_hash
+            ) VALUES (?, ?, ?, ?, ?, ?, ?)
+        ",
+        video_meta.blob_id,
+        video_meta.resolution,
+        video_meta.resolution_width,
+        video_meta.resolution_height,
+        video_meta.video_format,
+        video_meta.length,
+        video_meta.audio_hash,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_subtitle_metadata(
+    db: &Db,
+    subtitle_meta: &SubtitleMetadataItem,
+) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO subtitle_metadata (
+                blob_id,
+                video_blob_id,
+                language
+            ) VALUES (?, ?, ?)
+        ",
+        subtitle_meta.blob_id,
+        subtitle_meta.video_blob_id,
+        subtitle_meta.language,
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
+
+pub async fn insert_blob(db: &Db, blob: &BlobItem) -> Result<i64, sqlx::Error> {
+    let result = sqlx::query!(
+        "
+            INSERT INTO blobs (id, creation_time, mime_type, hash, filename)
+            VALUES (?, ?, ?, ?, ?);
+        ",
+        blob.id,
+        blob.creation_time,
+        blob.mime_type,
+        blob.hash,
+        blob.filename
+    )
+    .execute(db)
+    .await?;
+
+    return Ok(result.last_insert_rowid());
+}
