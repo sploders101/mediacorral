c9fe935d3d865ce790ce8e533f23c2a41fca7fe1
Shaun Keys <mariobuddy@gmail.com>
2025-06-27 07:53:05 -0600

    Fix job loading status on UI

--- frontend/src/pages/catalogue/[id].vue
+++ frontend/src/pages/catalogue/[id].vue
@@ -44,40 +44,42 @@ watch(
 		console.log(jobInfoResponse.response.details, catInfoResponse.response);
 
 		// Fetch relevant media
-		if (jobInfoResponse.response.details?.suspectedContents === undefined)
-			return;
-		const suspectedContents =
-			jobInfoResponse.response.details.suspectedContents.suspectedContents;
-		switch (suspectedContents.oneofKind) {
-			case "movie":
-				const { response: movie } = await rpc.getMovieByTmdbId({
-					tmdbId: suspectedContents.movie.tmdbId,
-				});
-				if (movie.movie === undefined) return;
-				cache.movies[movie.movie.id] = movie.movie;
-				break;
-			case "tvEpisodes":
-				const tvShows = new Set<string>();
-				const tvSeasons = new Set<string>();
-				for (const tmdbId of suspectedContents.tvEpisodes.episodeTmdbIds) {
-					const { response: tvEpisode } = await rpc.getTvEpisodeByTmdbId({
-						tmdbId: tmdbId,
+		if (jobInfoResponse.response.details?.suspectedContents !== undefined) {
+			const suspectedContents =
+				jobInfoResponse.response.details.suspectedContents.suspectedContents;
+			switch (suspectedContents.oneofKind) {
+				case "movie":
+					const { response: movie } = await rpc.getMovieByTmdbId({
+						tmdbId: suspectedContents.movie.tmdbId,
 					});
-					if (tvEpisode.episode === undefined) return;
-					cache.tvEpisodes[tvEpisode.episode.id] = tvEpisode.episode;
-					tvShows.add(tvEpisode.episode.tvShowId);
-					tvSeasons.add(tvEpisode.episode.tvSeasonId);
-				}
-				for (const tvId of tvShows) {
-					const { response: tvShow } = await rpc.getTvShow({ showId: tvId });
-					if (tvShow.tvShow === undefined) return;
-					cache.tvShows[tvShow.tvShow.id] = tvShow.tvShow;
-				}
-				for (const seasonId of tvSeasons) {
-					const { response: tvSeason } = await rpc.getTvSeason({ seasonId });
-					if (tvSeason.tvSeason === undefined) return;
-					cache.tvSeasons[tvSeason.tvSeason.id] = tvSeason.tvSeason;
-				}
+					if (movie.movie === undefined) throw new Error("Movie missing");
+					cache.movies[movie.movie.id] = movie.movie;
+					break;
+				case "tvEpisodes":
+					const tvShows = new Set<string>();
+					const tvSeasons = new Set<string>();
+					for (const tmdbId of suspectedContents.tvEpisodes.episodeTmdbIds) {
+						const { response: tvEpisode } = await rpc.getTvEpisodeByTmdbId({
+							tmdbId: tmdbId,
+						});
+						if (tvEpisode.episode === undefined)
+							throw new Error("TV episode missing");
+						cache.tvEpisodes[tvEpisode.episode.id] = tvEpisode.episode;
+						tvShows.add(tvEpisode.episode.tvShowId);
+						tvSeasons.add(tvEpisode.episode.tvSeasonId);
+					}
+					for (const tvId of tvShows) {
+						const { response: tvShow } = await rpc.getTvShow({ showId: tvId });
+						if (tvShow.tvShow === undefined) throw new Error("TV show missing");
+						cache.tvShows[tvShow.tvShow.id] = tvShow.tvShow;
+					}
+					for (const seasonId of tvSeasons) {
+						const { response: tvSeason } = await rpc.getTvSeason({ seasonId });
+						if (tvSeason.tvSeason === undefined)
+							throw new Error("TV season missing");
+						cache.tvSeasons[tvSeason.tvSeason.id] = tvSeason.tvSeason;
+					}
+			}
 		}
 		loading.value = false;
 	},
