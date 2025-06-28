4b6b5506a2858af69ea534c3ffdb1e30c7824462
Shaun Keys <mariobuddy@gmail.com>
2025-06-27 23:15:19 -0600

    Add rename, reload, and resolution to job viewer

--- frontend/src/pages/catalogue/[id].vue
+++ frontend/src/pages/catalogue/[id].vue
@@ -6,6 +6,7 @@ import type {
 	TvEpisode,
 	TvSeason,
 	TvShow,
+	VideoFile,
 } from "@/generated/mediacorral/server/v1/api";
 import { injectKeys } from "@/scripts/config";
 import { formatRuntime } from "@/scripts/utils";
@@ -31,60 +32,62 @@ const cache = reactive<MetaCache>({
 	tvEpisodes: {},
 });
 
-watch(
-	() => route.params.id,
-	async () => {
-		loading.value = true;
-		const jobInfoResponse = await rpc.getJobInfo({ jobId: route.params.id });
-		const catInfoResponse = await rpc.getJobCatalogueInfo({
-			jobId: route.params.id,
-		});
-		jobInfo.value = jobInfoResponse.response.details;
-		catInfo.value = catInfoResponse.response;
-		console.log(jobInfoResponse.response.details, catInfoResponse.response);
+watch(() => route.params.id, refreshData, { immediate: true });
 
-		// Fetch relevant media
-		if (jobInfoResponse.response.details?.suspectedContents !== undefined) {
-			const suspectedContents =
-				jobInfoResponse.response.details.suspectedContents.suspectedContents;
-			switch (suspectedContents.oneofKind) {
-				case "movie":
-					const { response: movie } = await rpc.getMovieByTmdbId({
-						tmdbId: suspectedContents.movie.tmdbId,
-					});
-					if (movie.movie === undefined) throw new Error("Movie missing");
+async function refreshData() {
+	loading.value = true;
+	const jobInfoResponse = await rpc.getJobInfo({ jobId: route.params.id });
+	const catInfoResponse = await rpc.getJobCatalogueInfo({
+		jobId: route.params.id,
+	});
+	jobInfo.value = jobInfoResponse.response.details;
+	catInfo.value = catInfoResponse.response;
+	console.log(jobInfoResponse.response.details, catInfoResponse.response);
+
+	// Fetch relevant media
+	if (jobInfoResponse.response.details?.suspectedContents !== undefined) {
+		const suspectedContents =
+			jobInfoResponse.response.details.suspectedContents.suspectedContents;
+		switch (suspectedContents.oneofKind) {
+			case "movie":
+				const { response: movie } = await rpc.getMovieByTmdbId({
+					tmdbId: suspectedContents.movie.tmdbId,
+				});
+				if (movie.movie === undefined) throw new Error("Movie missing");
+				if (cache.movies[movie.movie.id] === undefined) {
 					cache.movies[movie.movie.id] = movie.movie;
-					break;
-				case "tvEpisodes":
-					const tvShows = new Set<string>();
-					const tvSeasons = new Set<string>();
-					for (const tmdbId of suspectedContents.tvEpisodes.episodeTmdbIds) {
-						const { response: tvEpisode } = await rpc.getTvEpisodeByTmdbId({
-							tmdbId: tmdbId,
-						});
-						if (tvEpisode.episode === undefined)
-							throw new Error("TV episode missing");
-						cache.tvEpisodes[tvEpisode.episode.id] = tvEpisode.episode;
-						tvShows.add(tvEpisode.episode.tvShowId);
-						tvSeasons.add(tvEpisode.episode.tvSeasonId);
-					}
-					for (const tvId of tvShows) {
-						const { response: tvShow } = await rpc.getTvShow({ showId: tvId });
-						if (tvShow.tvShow === undefined) throw new Error("TV show missing");
-						cache.tvShows[tvShow.tvShow.id] = tvShow.tvShow;
-					}
-					for (const seasonId of tvSeasons) {
-						const { response: tvSeason } = await rpc.getTvSeason({ seasonId });
-						if (tvSeason.tvSeason === undefined)
-							throw new Error("TV season missing");
-						cache.tvSeasons[tvSeason.tvSeason.id] = tvSeason.tvSeason;
-					}
-			}
+				}
+				break;
+			case "tvEpisodes":
+				const tvShows = new Set<string>();
+				const tvSeasons = new Set<string>();
+				for (const tmdbId of suspectedContents.tvEpisodes.episodeTmdbIds) {
+					const { response: tvEpisode } = await rpc.getTvEpisodeByTmdbId({
+						tmdbId: tmdbId,
+					});
+					if (tvEpisode.episode === undefined)
+						throw new Error("TV episode missing");
+					cache.tvEpisodes[tvEpisode.episode.id] = tvEpisode.episode;
+					tvShows.add(tvEpisode.episode.tvShowId);
+					tvSeasons.add(tvEpisode.episode.tvSeasonId);
+				}
+				for (const tvId of tvShows) {
+					if (cache.tvShows[tvId] !== undefined) continue;
+					const { response: tvShow } = await rpc.getTvShow({ showId: tvId });
+					if (tvShow.tvShow === undefined) throw new Error("TV show missing");
+					cache.tvShows[tvShow.tvShow.id] = tvShow.tvShow;
+				}
+				for (const seasonId of tvSeasons) {
+					if (cache.tvSeasons[seasonId] !== undefined) continue;
+					const { response: tvSeason } = await rpc.getTvSeason({ seasonId });
+					if (tvSeason.tvSeason === undefined)
+						throw new Error("TV season missing");
+					cache.tvSeasons[tvSeason.tvSeason.id] = tvSeason.tvSeason;
+				}
 		}
-		loading.value = false;
-	},
-	{ immediate: true }
-);
+	}
+	loading.value = false;
+}
 
 const tableItems = computed(() => {
 	if (jobInfo.value === undefined || catInfo.value === undefined) return [];
@@ -93,13 +96,60 @@ const tableItems = computed(() => {
 		id: videoFile.id,
 		runtime:
 			videoFile.length === undefined ? "?" : formatRuntime(videoFile.length),
+		resolution: formatResolution(videoFile),
 	}));
 });
+
+async function renameJob() {
+	if (jobInfo.value === undefined) return;
+	const newName = prompt("New name:", jobInfo.value.discTitle);
+	if (newName === null) return;
+	loading.value = true;
+	await rpc.renameJob({
+		jobId: jobInfo.value.id,
+		newName,
+	});
+	const { response } = await rpc.getJobInfo({ jobId: jobInfo.value.id });
+	jobInfo.value = response.details;
+	loading.value = false;
+}
+
+function formatResolution(file: VideoFile) {
+	const formatted = `${file.resolutionWidth}x${file.resolutionHeight}`;
+	switch (formatted) {
+		case "720x480":
+			return "720p";
+		case "1920x1080":
+			return "1080p";
+		case "3840x2160":
+			return "2160p";
+		default:
+			return formatted;
+	}
+}
 </script>
 
 <template>
 	<div class="padding-small">
 		<v-card>
+			<v-card-title>
+				<div class="flex row">
+					{{ jobInfo?.discTitle }}
+					<v-btn
+						density="compact"
+						flat
+						icon="mdi-rename"
+						@click="renameJob()"
+					/>
+					<v-spacer />
+					<v-btn
+						density="compact"
+						flat
+						icon="mdi-reload"
+						@click="refreshData()"
+					/>
+				</div>
+			</v-card-title>
 			<v-card-text>
 				<v-data-table
 					:loading="loading"
@@ -107,6 +157,7 @@ const tableItems = computed(() => {
 					:headers="[
 						{ title: 'ID', value: 'id', sortable: false },
 						{ title: 'Runtime', value: 'runtime', sortable: false },
+						{ title: 'Resolution', value: 'resolution', sortable: false },
 					]"
 					hide-default-footer
 				/>
