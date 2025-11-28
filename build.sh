035755944d95fb0d98307e26c29a2877657bca49
Shaun Keys <mariobuddy@gmail.com>
2025-11-28 13:27:19 -0700

    Add build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+buf generate
+
+cd frontend
+npm run build
+cd -
+
+cd backend
+rm -r frontend/*
+rsync -a ../frontend/dist/ ./frontend/ || exit
+go build . || exit
+cd -
+
+cargo build --release || exit
+
+rm dist/*
+test -d dist || mkdir dist || exit
+mv backend/backend dist/backend || exit
+mv target/release/mediacorral-analysis-cli dist/analysis-cli || exit
+mv target/release/mediacorral-drive-controller dist/drive-controller || exit
