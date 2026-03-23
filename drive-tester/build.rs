c63bb58adbace220f0825bc40dca7fae96f9c6e3
Shaun Keys <mariobuddy@gmail.com>
2026-03-23 17:15:51 -0600

    Remove drive tester

--- drive-tester/build.rs
+++
@@ -1,8 +0,0 @@
-pub fn main() {
-    tonic_build::configure()
-        .compile_protos(
-            &["../proto/mediacorral/drive_coordinator/v1/main.proto"],
-            &["../proto/"],
-        )
-        .unwrap();
-}
--- drive-tester/src/proto.rs
+++
@@ -1,7 +0,0 @@
-pub mod mediacorral {
-    pub mod drive_coordinator {
-        pub mod v1 {
-            tonic::include_proto!("mediacorral.drive_coordinator.v1");
-        }
-    }
-}
